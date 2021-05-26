4e16a36fb3ca2869c5f0ae2738ced342bf736b71
Alyssa <aebowden36@gmail.com>
2021-05-25 22:55:59 -0400

    edited app

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import datetime as dt
 import numpy as np
-import pandas as pd
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -12,21 +11,22 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
+Base.classes.keys()
 
 # Save references to each table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 session = Session(engine)
-#################################################
-# Flask Setup
-#################################################
+################################################
+#Flask Setup
+################################################
 app = Flask(__name__)
 
 recent_date = (session.query(Measurement.date).order_by(Measurement.date.desc()).first())
@@ -45,7 +45,6 @@ year_earlier = dt.date(recent_year, recent_month, recent_day) - dt.timedelta(day
 
 @app.route("/")
 def welcome():
-
     return (
         f"Welcome to the Hawaii Climate Analysis API!<br/>"
         f"Available Routes:<br/>"
@@ -55,57 +54,65 @@ def welcome():
         f"/api/v1.0/temp/start/end"
     )
 
-
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     """Return the precipitation data for the last year"""
     
     session = Session(engine)
     results = session.query(Measurement.date, Measurement.prcp, Measurement.station).\
-                filter(Measurement.date >='2016-08-23').\
+                filter(Measurement.date >= year_earlier).\
                 order_by(Measurement.date).all()
-
+    session.close()
     prcp_data = []
     for result in results:
         prcp_dict = {result.date: result.prcp, 'Station': result.station}
         prcp_data.append(prcp_dict)
     return jsonify(prcp_data) 
-    session.close()
+    
     
 @app.route("/api/v1.0/stations")
 def stations():
     """Return a list of stations."""
     session = Session(engine)
     results = session.query(Station.name).all()
+    session.close()
     all_stations = list(np.ravel(results))
     return jsonify(all_stations)
-    session.close()
     
 @app.route("/api/v1.0/tobs")
 def temp_monthly():
     """Return the temperature observations (tobs) for previous year."""
     session = Session(engine)
     results = session.query(Measurement.date, Measurement.tobs, Measurement.station).\
-                filter(Measurement.date >= '2016-08-23').\
+                filter(Measurement.date >= year_earlier).\
                 order_by(Measurement.date).all()
-
+    session.close()
     temp_data = []
     for result in results:
         temp_dict = {result.date: result.temp, 'Station': result.station}
         temp_data.append(temp_dict)
     return jsonify(temp_data)
-    session.close()
 
 
 def stats(start=None, end=None):
     """Return TMIN, TAVG, TMAX."""
     session = Session(engine)
-    results = session.query().all() 
+    results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+                filter(func.strftime("%Y-%m-%d", Measurement.date) >= start).\
+                filter(func.strftime("%Y-%m-%d", Measurement.date) >= end).\
+                group_by(Measurement.date).all()
     session.close()
 
-   
-    return jsonify()
+    dates = []
+    for result in results:
+        date_dict = {}
+        date_dict["Date"] = result[0]
+        date_dict["Lowest Temp"] = result[1]
+        date_dict["Highest Temp"] = result[2]
+        date_dict["Average Temp"] = result[3]
+        dates.append(date_dict)
+    return jsonify(dates)
 
-
+session.close()
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
